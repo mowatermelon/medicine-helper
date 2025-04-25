5ab06fde579f3ede78922dda00a13e1b90a1d1f1
zaozao <neefoxmo@gmail.com>
2025-04-25 22:18:43 +0800

    update: remove unused code

--- src/components/MedicineForm.tsx
+++ src/components/MedicineForm.tsx
@@ -1,4 +1,4 @@
-import { useForm, useWatch } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Button } from '@/components/ui/button';
